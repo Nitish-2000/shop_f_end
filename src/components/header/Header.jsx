c3b1aefa0bc1c4fe1a470f81c5593983d4f091f9
“Nitish-2000” <nitishmani111@gmail.com>
2024-02-06 23:15:50 +0530

    header section updated

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,10 @@
 /* eslint-disable no-unused-vars */
 
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './Headers.module.scss'
 import { Link, NavLink } from 'react-router-dom'
+import {FaShoppingCart, FaTimes} from 'react-icons/fa'
+import {HiOutlineMenuAlt3} from 'react-icons/hi'
 
 
 export const logo = (
@@ -20,14 +22,47 @@ const activeLink =({isActive})=>(isActive? `${styles.active}`:""
 )
 
 function Header() {
+
+    const [showMenu, setShowMenu] = useState(false)
+
+    const toggleMenu = ()=>{
+            setShowMenu(!showMenu)
+    }
+
+    const hideMenu = ()=>{
+        setShowMenu(false)
+}
+
+    const cart = (
+        <span className={styles.cart}>
+            <Link to="/cart">
+            Cart
+            < FaShoppingCart size={20} />
+            <p>0</p>
+            </Link>
+        </span>
+    )
   return (
   <header>
     <div className={styles.header}>
         {logo}
-        <nav>
+        <nav className={showMenu? `${styles["show-nav"]}` : `${styles["hide-nav"]}`}>
+           <div className={showMenu? `${styles["nav-wrapper"]} ${styles["show-nav-wrapper"]}`
+            :
+             `${styles["nav-wrapper"]}`} onClick={hideMenu}>
+
+             </div>
+           
+           
             <ul>
+            <li className={styles["logo-mobile"]}>
+                  {logo}
+                  <FaTimes size={22} menucolor="#fff" onClick={hideMenu} />
+            </li>
             <li>
-                <NavLink to="/shop" className={activeLink}> Shop </NavLink>
+                <NavLink to="/shop" className={activeLink}>
+                 Shop
+                  </NavLink>
             </li>
 
             </ul>
@@ -43,8 +78,13 @@ function Header() {
                         My Order
                     </NavLink>
                 </span>
+                {cart}
             </div>
         </nav>
+        <div className={styles["menu-icon"]}>
+            {cart}
+            <HiOutlineMenuAlt3 size={28} onClick={toggleMenu} />
+        </div>
     </div>
   </header>
   )
